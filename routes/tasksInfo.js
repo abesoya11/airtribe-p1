c06012d1995ad61f25e8e6c396301fcd6d6c063d
Aditya besoya <abesoya11@gmail.com>
2023-06-24 16:50:30 +0530

    all api done

--- routes/tasksInfo.js
+++ routes/tasksInfo.js
@@ -49,6 +49,29 @@ tasksRoutes.delete("/:id", (req, res) => {
   res.status(200).send(result);
 });
 
+tasksRoutes.put("/:id", (req, res) => {
+  let updateTask = req.body;
+  let tasks = tasksData.tasks;
+  updateTask.id = req.params.id;
+
+  let taskId = req.params.id;
+  let result = tasks.filter((T) => {
+    console.log("inside filter with t.id is " + T.id);
+    console.log("task id is  " + taskId);
+    if (T.id != taskId) {
+      console.log("hit");
+      return T;
+    }
+  });
+
+  result.push(updateTask);
+
+  let writePath = path.join(__dirname, "..", "taskManagerData");
+  fs.writeFileSync(writePath, JSON.stringify(result), { encoding: "utf8", flag: "w" });
+
+  res.status(200).send(result);
+});
+
 tasksRoutes.get("/:id", (req, res) => {
   let tasks = tasksData.tasks;
   let taskId = req.params.id;
